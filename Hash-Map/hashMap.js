4a97d1d6684bd175ec5a564b7d98ddcbc6037d66
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-07-17 18:12:19 +0800

    create and test set(key, value) function in HashMap, that sets a new key pair value in hash map

--- Hash-Map/hashMap.js
+++ Hash-Map/hashMap.js
@@ -5,6 +5,7 @@ class HashMap {
     this.loadFactor = loadFactor;
     this.capacity = capacity;
     this.buckets = new Array(this.capacity).fill(new LinkedList());
+    this.entries = [];
   }
 
   // hash(key) takes a key and produces a hash code with it
@@ -16,6 +17,32 @@ class HashMap {
       hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
     return hashCode;
+  }
+
+  // set(key, value) will set a new key value pair in buckets
+  set(key, value) {
+    // get the index of the bucket that will store the key value pair
+    const index = this.hash(key);
+
+    // if buckets has reached load factor, double buckets capacity
+    if (this.entries + 1 >= this.capacity * this.loadFactor) {
+      this.growBuckets();
+    }
+    // if key already exists, update the key's value, else just append new key value pair to bucket and entries
+    if (this.buckets[index].containsKey(key)) {
+      const keyIndex = this.buckets[index].findKey(key);
+      this.buckets[index].removeAt(keyIndex);
+      this.buckets[index].insertAt(keyIndex, { [key]: value });
+
+      // update entries also
+      // get the index of the existsing key value pair in entries and update the entry's value
+      const entryIndex = this.entries.findIndex((e) => e.hasOwnProperty(key));
+      this.entries[entryIndex] = { [key]: value };
+      return;
+    }
+
+    this.buckets[index].append({ [key]: value });
+    this.entries.push({ [key]: value });
   }
 }
 
--- Hash-Map/hashMap.test.js
+++ Hash-Map/hashMap.test.js
@@ -3,3 +3,24 @@ import {HashMap} from "./hashMap.js"
 test("HashMap.hash(key) takes a key and returns a hash code with it", () => {
   expect(new HashMap().hash("Manon")).toBe(9);
 });
+
+test("HashMap.set(key, value) sets a new key value pair", () => {
+  const h = new HashMap();
+  h.set("first", "apple");
+  expect(h.buckets[0].head()).toEqual({ first: "apple" });
+});
+
+test("HashMap.set(key, value) updates key's value if key already exists in bucket", () => {
+  const h = new HashMap();
+  h.set("first", "apple");
+  h.set("first", "banana");
+  expect(h.buckets[0].head()).toEqual({ first: "banana" });
+});
+
+test("HashMap.set(key, value) appends new key value pair to list if list is not empty", () => {
+  const h = new HashMap();
+  h.set("Rama", "apple");
+  h.set("Sita", "banana");
+  expect(h.buckets[3].head()).toEqual({ Rama: "apple" });
+  expect(h.buckets[3].tail()).toEqual({ Sita: "banana" });
+});
